b98c34da7702beecfb0e8297d8c70e6825e1365e
zhengdong-654 <zhengdong@huahongjt.com>
2022-11-28 09:47:39 +0800

    zhengdong-654-2022-11-28-add

---
+++ Work/work/TPDownload/serial.h
@@ -0,0 +1,20 @@
+//serial
+#ifndef _SERIAL_
+#define _SERIAL_
+
+//start of file
+
+#define PROT_DT_STX   0x02
+#define PROT_DT_ETX   0x03
+#define PROT_DT_DLE   0x10
+
+int serial(char * port, unsigned char *inbuf, int inlen, unsigned char *outbuf, int outlen, short timeMs);
+int InsertXOR(unsigned char *pbytData, int intLength);
+int DeleteXOR(unsigned char *pbytData, int intLength);
+unsigned char writecom(int fd, unsigned char *pdata, long lnglen);
+unsigned char readcom(int fd, unsigned char *pdata, long lnglen);
+void speed_set(int fd, int speed);
+int parity_set(int fd, int databits, int stopbits, int parity);
+
+//end of file
+#endif
---
+++ Work/work/TPDownload/sz.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "run the TPdownload application...."
+chmod 700 * -R
+./TPdownload /dev/ttyS0
